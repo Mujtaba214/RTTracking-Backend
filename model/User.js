1b4e6019f770bd0770cc6529361e229362c4b002
Muhammad Mujtaba <mujtabausman21@gmail.com>
2025-12-22 02:17:39 +0500

    updated model

---
+++ model/User.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  coordinates: {
+    lat: {
+      type: Number,
+      required: true,
+    },
+    lng: {
+      type: Number,
+      required: true,
+    },
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
